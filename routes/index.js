1df992166ad8cec2dcb86ab74f6bb821bf3d8288
Deepak Bawa <bawa_d@ymail.com>
2019-03-05 15:38:27 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const authRouter = require('./auth');
+const rolesRouter = require('./role');
+const usersRouter = require('./user');
+const courseTypesRouter = require('./course_type');
+const coursesRouter = require('./course');
+
+router.use('/auth', authRouter);
+router.use('/roles', rolesRouter);
+router.use('/users', usersRouter);
+router.use('/courseTypes', courseTypesRouter);
+router.use('/courses', coursesRouter);
 
 module.exports = router;
